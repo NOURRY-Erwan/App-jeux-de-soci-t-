ef6cb470274f405b92d17b12b260165ca2f4209b
NOURRY-Erwan <servicejeunesse.carantec@gmail.com>
2024-11-17 18:27:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from typing import Optional
 import re
 
 # Constante pour le lien Google Sheets
-SHEET_URL = https://docs.google.com/spreadsheets/d/1itKcj2L9HyA0GBIFcRTeQ8-OiIOI5eqw23-vvgXI5pQ/edit?gid=1409318024#gid=1409318024  # Remplacez par votre lien
+SHEET_URL = https://docs.google.com/spreadsheets/d/1itKcj2L9HyA0GBIFcRTeQ8-OiIOI5eqw23-vvgXI5pQ/edit?usp=sharing # Remplacez par votre lien
 
 def setup_page():
     """Configure la page Streamlit avec les paramètres initiaux."""
